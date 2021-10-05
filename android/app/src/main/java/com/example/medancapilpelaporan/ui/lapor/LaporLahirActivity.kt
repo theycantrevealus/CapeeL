394c0e1e2562bf7f9a40dc915bea2725de5f5f4c
Hendry Tanaka <theycantrevealus@gmail.com>
2021-10-05 12:04:02 +0700

    Latest

--- android/app/src/main/java/com/example/medancapilpelaporan/ui/lapor/LaporLahirActivity.kt
+++ android/app/src/main/java/com/example/medancapilpelaporan/ui/lapor/LaporLahirActivity.kt
@@ -1,6 +1,7 @@
 package com.example.medancapilpelaporan.ui.lapor
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.medancapilpelaporan.databinding.ActivityLaporLahirBinding
 
@@ -12,6 +13,19 @@ class LaporLahirActivity: AppCompatActivity() {
 
         binding = ActivityLaporLahirBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.laporLahirTempatLahir.setOnClickListener{
+            //
+        }
+
+        binding.btnLaporkanLahir.setOnClickListener {
+            val nama = binding.laporLahirNamaAnak.text.toString().trim()
+            val tempat_lahir = binding.laporLahirTempatLahir.text.toString().trim()
+            val tanggal_lahir = binding.laporLahirTanggalLahir.text.toString().trim()
+            val nik_ortu = binding.laporLahirNikOrtu.text.toString().trim()
+            val nama_ortu = binding.laporLahirNamaOrtu.text.toString().trim()
+            val alamat = binding.laporLahirAlamat.text.toString().trim()
+        }
 
     }
 }
